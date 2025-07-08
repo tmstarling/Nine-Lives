3f7f950fa860ccf49598c5682e72c59e993d3f89
Tevin Starling <TMStarling@student.fullsail.edu>
2025-07-08 12:16:19 -0400

    Added gameManager

--- Assets/Scripts/GameManger.cs
+++ Assets/Scripts/GameManger.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
 
-public class GameManger : MonoBehaviour
+public class gameManger : MonoBehaviour
 {
+    public static gameManger instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPaused;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+
+
+    public bool isPaused = false;
+    public GameObject player;
+    public PlayerController playerScript;
+
+    float timescaleOrig;
+
+    int gameGoalCount;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
+        instance = this;
+
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<PlayerController>();
+        timescaleOrig = Time.timeScale;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            isPaused = isPaused;
+            menuPaused.SetActive(isPaused);
+        }
         
     }
 }
