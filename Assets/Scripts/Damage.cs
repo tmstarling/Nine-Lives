2c418954dfe65373f53f4ee6286522223d3a629d
Tevin Starling <tmstarling@student.fullsail.edu>
2025-07-10 02:29:58 -0400

    Added UI to prefab
    quit restart button added

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -10,6 +10,7 @@ public class Damage : MonoBehaviour
     [SerializeField] int DamageAmount;
     [SerializeField] int Speed;
     [SerializeField] int DestroyTime;
+    [SerializeField] float damageRate;
 
     bool isDamaging;
 
